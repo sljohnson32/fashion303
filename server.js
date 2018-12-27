6269c0fe2ed38970262bf687ed26b03e57117278
sljohnson32 <sljohnson32@gmail.com>
2018-12-27 13:40:59 -0700

    Adds new code for server

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
-app.use(express.static(path.resolve(__dirname, './build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.set('port', process.env.PORT || 3001);
-
-app.get('/', (request, response) => {
-  response.readFile(path.resolve(__dirname, './build', 'index.html', (err, file) => {
-    response.send(file);
-  }));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
